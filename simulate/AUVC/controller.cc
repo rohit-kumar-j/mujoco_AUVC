5e764088a1c9cc6be4be062c1b01ce7f1286f3f6
rohit-kumar-j <jrohit1110@gmail.com>
2024-09-19 14:00:40 -0400

    Driving Config 1

--- simulate/AUVC/controller.cc
+++ simulate/AUVC/controller.cc
@@ -58,7 +58,9 @@ float clip(float val, float min, float max) {
 void set_props_speed(mjData *d, float speed);
 void set_hinge_joints_zero(mjData *d);
 void single_leg_pattern(mjData *d);
-void fold_legs(mjData*d);
+void fold_legs(mjData *d);
+void drive_config_1(mjData *d);
+void drive_with_props_1(mjData *d, float speed);
 
 extern "C" bool controllerUpdatePlug(mjModel *m, mjData *d) {
 
@@ -67,14 +69,20 @@ extern "C" bool controllerUpdatePlug(mjModel *m, mjData *d) {
   // single_leg_pattern(d);
 
   // Flying section!
-  float speed = d->time < 10.0 ? d->time * 17 * 10 : 1700.0;
+  // float speed = d->time < 10.0 ? d->time * 17 * 10 : 1700.0;
   // printf("speed: %f\n", speed);
   // set_hinge_joints_zero(d);
   // set_props_speed(d,speed);
 
   // Flying with legs closed!
-  fold_legs(d);
-  set_props_speed(d, speed);
+  // float speed = d->time < 10.0 ? d->time * 17 * 10 : 1700.0;
+  // fold_legs(d);
+  // set_props_speed(d, speed);
+
+  // Driving configuration 1
+  float speed = d->time < 10.0 ? d->time * 1 * 10 : 100.0;
+  drive_config_1(d);
+  drive_with_props_1(d, speed);
 
   return true;
 }
@@ -95,31 +103,22 @@ void set_hinge_joints_zero(mjData *d) {
   d->ctrl[17] = 0.0; // bl_3_act
 }
 
-void fold_legs(mjData*d){
-    d->ctrl[0] = 0.0;
-    d->ctrl[1] = 0.0;
-    d->ctrl[2] = 70.0;
+void fold_legs(mjData *d) {
+  d->ctrl[0] = 0.0;
+  d->ctrl[1] = 0.0;
+  d->ctrl[2] = 70.0;
 
-    d->ctrl[5] = 0.0;
-    d->ctrl[6] = 0.0;
-    d->ctrl[7] = 70.0;
+  d->ctrl[5] = 0.0;
+  d->ctrl[6] = 0.0;
+  d->ctrl[7] = 70.0;
 
-    d->ctrl[10] = 0.0;
-    d->ctrl[11] = 0.0;
-    d->ctrl[12] = 70.0;
+  d->ctrl[10] = 0.0;
+  d->ctrl[11] = 0.0;
+  d->ctrl[12] = 70.0;
 
-    d->ctrl[15] = 0.0;
-    d->ctrl[16] = 0.0;
-    d->ctrl[17] = 70.0;
-
-    // d->ctrl[3] = -speed;  // fr_prop force
-    // d->ctrl[4] = speed;   // fr_prop
-    // d->ctrl[8] = speed;   // fl_prop force
-    // d->ctrl[9] = speed;   // fl_prop
-    // d->ctrl[13] = speed;  // br_prop force
-    // d->ctrl[14] = speed;  // br_prop
-    // d->ctrl[18] = -speed; // bl_prop force
-    // d->ctrl[19] = speed;  // bl_prop
+  d->ctrl[15] = 0.0;
+  d->ctrl[16] = 0.0;
+  d->ctrl[17] = 70.0;
 }
 
 void set_props_speed(mjData *d, float speed) {
@@ -147,57 +146,62 @@ void single_leg_pattern(mjData *d) {
     mul = -1;
   }
   theta += 0.002;
-  float dt = (5.0*M_PI)/2.0;
-  if(0<=theta && theta<= dt){
+  float dt = (5.0 * M_PI) / 2.0;
+  if (0 <= theta && theta <= dt) {
     printf("p1: %f/ %f \n", theta, dt);
     d->ctrl[0] = sin(theta) * -target_hip;                           // fr_1_act
     d->ctrl[1] = clip(sin(theta - 0.7) * target_knee, -2.0, 4.0);    // fr_2_act
     d->ctrl[2] = clip(sin(theta - 0.7) * target_foot, -15.0, -10.0); // fr_3_act
   }
-  if(dt<=theta && theta<=2*dt){
-    printf("p2: %f/ %f \n", theta, 2*dt);
-    d->ctrl[15] = sin(theta) * target_hip;                              // bl_1_act
-    d->ctrl[16] = clip(sin(theta - 0.7) * -target_knee, -2.0, 4.0);    // bl_2_act
-    d->ctrl[17] = clip(sin(theta - 0.7) * -target_foot, -15.0, -10.0); // bl_3_act
+  if (dt <= theta && theta <= 2 * dt) {
+    printf("p2: %f/ %f \n", theta, 2 * dt);
+    d->ctrl[15] = sin(theta) * target_hip;                          // bl_1_act
+    d->ctrl[16] = clip(sin(theta - 0.7) * -target_knee, -2.0, 4.0); // bl_2_act
+    d->ctrl[17] =
+        clip(sin(theta - 0.7) * -target_foot, -15.0, -10.0); // bl_3_act
   }
-  if(2*dt<=theta && theta<=3*dt){
-    printf("p3: %f/ %f \n", theta, 3*dt);
-    d->ctrl[5] = sin(theta ) * -target_hip;                          // fl_1_act
+  if (2 * dt <= theta && theta <= 3 * dt) {
+    printf("p3: %f/ %f \n", theta, 3 * dt);
+    d->ctrl[5] = sin(theta) * -target_hip;                          // fl_1_act
     d->ctrl[6] = clip(sin(theta - 0.7) * target_knee, -7.0, 0.0);   // fl_2_act
     d->ctrl[7] = clip(sin(theta - 0.7) * -target_foot, -15.0, 0.0); // fl_3_act
   }
-  if(3*dt<=theta && theta<=4*dt){
-    printf("p4: %f/ %f \n", theta, 4*dt);
-    d->ctrl[10] = sin(theta ) * -target_hip;                          // br_1_act
+  if (3 * dt <= theta && theta <= 4 * dt) {
+    printf("p4: %f/ %f \n", theta, 4 * dt);
+    d->ctrl[10] = sin(theta) * -target_hip;                          // br_1_act
     d->ctrl[11] = clip(sin(theta - 0.7) * target_knee, -7.0, 0.0);   // br_2_act
     d->ctrl[12] = clip(sin(theta - 0.7) * -target_foot, -15.0, 0.0); // br_2_act
   }
-  if(theta>=4*dt)
+  if (theta >= 4 * dt)
     theta = 0;
 
-
   // if(0 <= theta && theta <= M_PI){
-  //   d->ctrl[0] = sin(theta) * -target_hip;                           // fr_1_act
-  //   d->ctrl[1] = clip(sin(theta - 0.7) * target_knee, -2.0, 4.0);    // fr_2_act
-  //   d->ctrl[2] = clip(sin(theta - 0.7) * target_foot, -15.0, -10.0); // fr_3_act
+  //   d->ctrl[0] = sin(theta) * -target_hip;                           //
+  //   fr_1_act d->ctrl[1] = clip(sin(theta - 0.7) * target_knee, -2.0, 4.0); //
+  //   fr_2_act d->ctrl[2] = clip(sin(theta - 0.7) * target_foot, -15.0, -10.0);
+  //   // fr_3_act
   // }
   // if(M_PI <= theta && theta <= 2.0 * M_PI){
-  //   d->ctrl[15] = sin(theta+ M_PI) * target_hip;                             // bl_1_act
-  //   d->ctrl[16] = clip(sin(theta + M_PI - 0.7) * -target_knee, -2.0, 4.0);    // fr_2_act
-  //   d->ctrl[17] = clip(sin(theta + M_PI - 0.7) * -target_foot, -15.0, -10.0); //fr_3_act
+  //   d->ctrl[15] = sin(theta+ M_PI) * target_hip; // bl_1_act d->ctrl[16] =
+  //   clip(sin(theta + M_PI - 0.7) * -target_knee, -2.0, 4.0);    // fr_2_act
+  //   d->ctrl[17] = clip(sin(theta + M_PI - 0.7) * -target_foot, -15.0, -10.0);
+  //   //fr_3_act
   // }
   // if(M_PI <= theta && theta <= 2.0 * M_PI){
-  //   d->ctrl[5] = sin(theta + 2.0*M_PI ) * -target_hip;                          // fl_1_act
-  //   d->ctrl[6] = clip(sin(theta + 2.0*M_PI  - 0.7) * target_knee, -7.0, 0.0);   // fl_2_act
-  //   d->ctrl[7] = clip(sin(theta + 2.0*M_PI  - 0.7) * -target_foot, -15.0, 0.0); // fl_2_act
+  //   d->ctrl[5] = sin(theta + 2.0*M_PI ) * -target_hip; // fl_1_act d->ctrl[6]
+  //   = clip(sin(theta + 2.0*M_PI  - 0.7) * target_knee, -7.0, 0.0);   //
+  //   fl_2_act d->ctrl[7] = clip(sin(theta + 2.0*M_PI  - 0.7) * -target_foot,
+  //   -15.0, 0.0); // fl_2_act
   // }
   // //
   // if(M_PI <= theta && theta <= 3.0 * M_PI){
-  //   d->ctrl[10] = sin(theta + 3.0* M_PI) * -target_hip;                          // br_1_act
-  //   d->ctrl[11] = clip(sin(theta + 3.0* M_PI - 0.7) * target_knee, -7.0, 0.0);   // br_2_act
-  //   d->ctrl[12] = clip(sin(theta + 3.0* M_PI - 0.7) * -target_foot, -15.0, 0.0); // br_2_act
+  //   d->ctrl[10] = sin(theta + 3.0* M_PI) * -target_hip; // br_1_act
+  //   d->ctrl[11] = clip(sin(theta + 3.0* M_PI - 0.7) * target_knee, -7.0,
+  //   0.0);   // br_2_act d->ctrl[12] = clip(sin(theta + 3.0* M_PI - 0.7) *
+  //   -target_foot, -15.0, 0.0); // br_2_act
   // }
-  // printf("clipped: %f\n", d->ctrl[12]);                              // fr_2_act
+  // printf("clipped: %f\n", d->ctrl[12]);                              //
+  // fr_2_act
 
   // printf("ctrl[0] : %f | 15: %f | theta : %f\n", d->ctrl[0], d->ctrl[15],
   //        theta);
@@ -245,4 +249,33 @@ void single_leg_pattern(mjData *d) {
   // printf("bl_hip: %f\n", d->sensordata[9] * (180.0/M_PI));
   // printf("bl_knee: %f\n", d->sensordata[10] * (180.0/M_PI));
   // printf("bl_foot: %f\n", d->sensordata[11] * (180.0/M_PI));
+}
+
+void drive_config_1(mjData *d) {
+  d->ctrl[0] = -45;
+  d->ctrl[1] = -90.0;
+  d->ctrl[2] = 0.1;
+
+  d->ctrl[5] = 45;
+  d->ctrl[6] = 90.0;
+  d->ctrl[7] = 0.1;
+
+  d->ctrl[10] = 45;
+  d->ctrl[11] = 90.0;
+  d->ctrl[12] = 0.1;
+
+  d->ctrl[15] = -45;
+  d->ctrl[16] = 90.0;
+  d->ctrl[17] = 0.1;
+}
+
+void drive_with_props_1(mjData *d, float speed) {
+  d->ctrl[3] = -speed; // fr_prop force
+  // d->ctrl[4] = speed;   // fr_prop
+  d->ctrl[8] = speed; // fl_prop force
+  // d->ctrl[9] = speed;   // fl_prop
+  d->ctrl[13] = -speed; // br_prop force
+  // d->ctrl[14] = -speed; // br_prop
+  d->ctrl[18] = speed; // bl_prop force
+  // d->ctrl[19] = speed;  // bl_prop
 }
